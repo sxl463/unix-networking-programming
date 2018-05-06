c7fb33108c5701364dfef212bf906b4c5311528f
shen_liu <sxl463@gmail.com>
2018-05-05 22:56:13 -0400

    first commit

---
+++ src/sunrpc/test/server.c
@@ -0,0 +1,15 @@
+//#include	"unpipc.h"
+#include	"square.h"
+#include <stdio.h>
+
+square_out *
+squareproc_1_svc(square_in *inp, struct svc_req *rqstp)
+{
+	static square_out	out;
+
+	out.res1 = inp->arg1 * inp->arg1;
+
+	printf("server : %d\n", out.res1);
+	printf("server : %c\n", inp->arg2c);
+	return(&out);
+}
